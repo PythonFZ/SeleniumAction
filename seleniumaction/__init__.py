a7cfde6619027321a5709df3302d8066f09904a5
PythonFZ <fabian.zills@web.de>
2023-11-24 12:25:17 +0100

    working

--- seleniumaction/__init__.py
+++ seleniumaction/__init__.py
@@ -2,18 +2,19 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import dataclasses
 import threading
+import uuid
 
 
 def create_app() -> (Flask, SocketIO):
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "secret!"
+    app.config["SECRET_KEY"] = uuid.uuid4().hex
 
     socketio = SocketIO(app)
     
     @app.route("/")
     def index():
         return render_template("index.jinja2")
-    
+        
     @socketio.on("connect")
     def on_connect():
         print("connected")
--- tests/test_web01.py
+++ tests/test_web01.py
@@ -5,45 +5,78 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import socketio
+import time
 import random
-import copy
+import socketio
+import urllib.request
 
-# PORT = copy.deepcopy(int(random.randint(5000, 6000)))
-PORT = 5322
+COUNTER = 0
 
 @pytest.fixture()
-def setup():
-    app, socketio = create_app()
-
-    server_proc = mp.Process(
-        target=socketio.run,
-        args=(app,),
-        kwargs={
-            "debug": True,
-            "port": PORT,
-            "host": "0.0.0.0",
-        },
-    )
-    server_proc.start()
-
+def setup(request):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    yield driver
-    driver.close()
+    request.cls.driver = driver
+    yield request.cls.driver
+    request.cls.driver.close()
+
+
+@pytest.fixture()
+def server():
+
+    # port = random.randint(10000, 20000)
+    global COUNTER
+    port = 1234 + COUNTER
+    COUNTER += 1
+
+    # app, socketio = create_app()
+    # app.config.update({
+    #     "TESTING": False,
+    # })
+    
+    def run_server():
+        app, socketio = create_app()
+        # app.config.update({
+        #     "TESTING": False,
+        # })
+        socketio.run(app, port=port)
+
+    # server_proc = mp.Process(
+    #     target=socketio.run,
+    #     args=(app,),
+    #     kwargs={
+    #         "debug": True,
+    #         "port": port,
+    #         "host": "0.0.0.0",
+    #     },
+    # )
+
+    server_proc = mp.Process(
+        target=run_server,
+    )
+
+    server_proc.start()
+    time.sleep(5)
+    yield f"http://localhost:{port}"
     server_proc.terminate()
+    server_proc.join()
 
-def test_title(setup):
-    driver = setup
-    driver.get(f"http://localhost:{PORT}")
-    assert driver.title == "Flask SocketIO Example"
-    # get a div element with id="content"
-    content = driver.find_element(By.ID,"content")
-    # get the text from the div element
-    assert content.text == "JS generated content"
 
-# def test_connect_socketio(setup):
-#     client = socketio.Client()
-#     client.connect(f"http://localhost:{PORT}")
-#     assert client.connected
+@pytest.mark.usefixtures("setup")
+class TestExampleOne:
+
+    def test_title(self, server):
+        self.driver.get(server)
+        assert self.driver.title == "Flask SocketIO Example"
+        # get a div element with id="content"
+        content = self.driver.find_element(By.ID,"content")
+        # get the text from the div element
+        assert content.text == "JS generated content"
+    
+    def test_socketio_connect(self, server):
+        client = socketio.Client()
+        client.connect(server)
+        assert client.connected == True
+
+
